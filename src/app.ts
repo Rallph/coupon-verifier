f9b9249f87e5216f93567040801a201300d2e9c8
Ralph Nahra <ralph-nahra@uiowa.edu>
2020-11-16 22:33:19 -0600

    Add coupon code endpoint to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
+import couponRouter from './routers/CouponRouter';
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World");
-});
+app.use('/coupon', couponRouter);
 
 app.listen(5000, () => { console.log('Server running') });
