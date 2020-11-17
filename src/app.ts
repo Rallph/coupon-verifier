445e4566fd997f507a0da612ade04656376b9712
Ralph Nahra <ralph-nahra@uiowa.edu>
2020-11-17 04:41:05 -0600

    Make backend serve built frontend

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express, { Application, Request, Response } from 'express';
+import path from 'path';
 import couponRouter from './routers/CouponRouter';
 
 const app: Application = express();
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.use('/coupon', couponRouter);
 
-app.listen(5000, () => { console.log('Server running') });
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => { console.log('Server running') });
