e5a75713f1bc94814f12895de109b74d264027b3
Ralph Nahra <ralph-nahra@uiowa.edu>
2020-11-17 01:32:39 -0600

    Implement basic layout of app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import CouponValidator from './components/CouponValidator';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App m-3">
+      <Jumbotron>
+          <h1>Coupon Check</h1>
+          <p>Check the validity of a coupon code</p>
+          <CouponValidator/>
+      </Jumbotron>
     </div>
   );
 }
